580d366a503802cf32a05f49f2dca03cf042fe88
han-thinzaraung <hanthinzaraung@gicjp.com>
2024-03-04 16:07:23 +0630

    type-aliase

--- src/index.ts
+++ src/index.ts
@@ -125,3 +125,32 @@ createPost(newPost);
 
 let posts: Post[] = []
 posts.push(newPost);
+
+
+//type aliases
+type Rgb = [number, number, number];
+
+function getRandomColor(): Rgb{
+    const r = Math.floor(Math.random() * 255)
+    const g = Math.floor(Math.random() * 255)
+    const b = Math.floor(Math.random() * 255)
+
+    return [r,g,b]
+}
+const colorOne =  getRandomColor();
+const colorTwo = getRandomColor();
+console.log(colorOne,colorTwo);
+
+type User = {
+    name:string,
+    score:number
+}
+const userOne:User = {
+    name:'mario',
+    score:75
+}
+function formatUser(user:User): void {
+    console.log(`${user.name} has a score of ${user.score}`)
+}
+formatUser(userOne);
+formatUser({name : 'yoshi' , score : 45});
