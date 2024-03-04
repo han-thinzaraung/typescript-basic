70df433050ee20ed390c284a713836b16e74549d
han-thinzaraung <hanthinzaraung@gicjp.com>
2024-03-04 13:11:02 +0630

    variables-objects-array-declaration

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+let age = 30;
+let person = 'Mg Mg';
+let single = true;
+let fruits = ['apple', 'orange', 'pineapple'];
+fruits.push('Mg Mg');
+console.log(fruits);
+let ages = [20, 30, 40, 50, 60];
+ages.push(80);
+ages.push('Hla');
+console.log(ages);
+let myself = {
+    name: 'Kyaw Kyaw',
+    age: 22,
+    married: false
+};
+// myself.age = 'Su Su';
+console.log(myself);
